2105a0c25ad6e95d337ad49c531e2ed8803b0b43
Ling-Jyh Chen <cclljj@gmail.com>
2015-09-25 10:47:25 +0800

    1. changes of the wifi settings, battery testing, and minor format issues 2. moving WIFI AP/PASSWD and ThingSpeak Key information to a personal library (My_Password.h)

--- LJsensor-LinkIt/LJsensor-linkit.ino
+++ LJsensor-LinkIt/LJsensor-linkit.ino
@@ -1,10 +1,9 @@
+#include <My_Password.h>    // containing personal wifi/thingspeak/etc password settings
+
+#include <Time.h>
 #include <LWiFi.h>
 #include <LWiFiClient.h>
-//#include <LWiFiServer.h>
-//#include <LWiFiUdp.h>
-
-
-
+#include <LBattery.h>
 #include <DHT_linkit.h>
 #include <Digital_Light_TSL2561.h>
 #include <HP20x_dev.h>
@@ -13,18 +12,30 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-//#include <SPI.h>
-//#include <WiFi.h>
 
-// Grove Kit Sensor: temperature 
-//const int pinTemp = A1;.
-//const int B = 3975;
+#ifndef _THINGSPEAK_API_KEY_
+#define THINGSPEAK_API_KEY "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#endif
 
-// Grove Kit Sensor: light 
-//const int pinLight = A0;
+#ifndef _WIFI_
+#define WIFI_AP "default_AP"            // replace your WiFi AP SSID
+#define WIFI_PASSWORD "default_password"  // replace your WiFi AP password
+#define WIFI_AUTH LWIFI_OPEN           // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP according to your AP
+#endif
+
+
+LWiFiClient client;
+LWifiStatus wifistatus;
+const int updateThingSpeakInterval = 30 * 1000;      // Time interval in milliseconds to update ThingSpeak (number of seconds * 1000 = interval)
+// Variable Setup
+long lastConnectionTime = 0; 
+boolean lastConnected = false;
+int failedCounter = 0;
+
+
 
 // Grove - Temperature and Humidity Sensor Pro
-// Interface: Analog
+// Interface: Analog (or Digital)
 // http://www.seeedstudio.com/wiki/Grove_-_Temperature_and_Humidity_Sensor_Pro
 // for LinkIt One, use the library: https://github.com/Seeed-Studio/Grove_Starter_Kit_For_LinkIt/tree/master/libraries/Humidity_Temperature_Sensor
 #define DHTPIN 2     // what pin we're connected to
@@ -35,9 +46,7 @@ DHT_linkit dht(DHTPIN, DHTTYPE);
 // Grove - Barometer (High-Accuracy)
 // Interface: I2C
 // http://www.seeedstudio.com/wiki/Grove_-_Barometer_(High-Accuracy)
-
 unsigned char ret = 0;
-/* Instance */
 KalmanFilter t_filter;    //temperature filter
 KalmanFilter p_filter;    //pressure filter
 KalmanFilter a_filter;    //altitude filter
@@ -50,57 +59,34 @@ rgb_lcd lcd;
 
 
 
-// LinkIt One WiFi
-#define WIFI_AP "mymaktub"            // replace your WiFi AP SSID
-#define WIFI_PASSWORD "27858757"  // replace your WiFi AP password
-#define WIFI_AUTH LWIFI_WPA           // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP according to your AP
-
-LWiFiClient c;
-
-
 void setup()
 {
-    Serial.begin(9600);
     lcd.begin(16, 2);
-    // Configure the serial communication line at 9600 baud (bits per second.)
-
-    // Grove - Barometer (High-Accuracy)
-    // Power up,delay 150ms,until voltage is stable
-    delay(150);
-    // Reset HP20x_dev
-    HP20x.begin();
-    delay(100);
-  
-    /* Determine HP20x_dev is available or not */
-    ret = HP20x.isAvailable();
-    if(OK_HP20X_DEV == ret)
-    {
-      Serial.println("HP20x_dev is available.\n");    
-    }
-    else
-    {
-      Serial.println("HP20x_dev isn't available.\n");
-    }
+    Serial.begin(9600);
 
     // Grove - Temperature and Humidity Sensor Pro
     dht.begin();
 
     // Grove - Digital Light Sensor
     TSL2561.init();
+
+    // Grove - Barometer (High-Accuracy)
+    // Power up,delay 150ms,until voltage is stable
+    delay(150);
+    HP20x.begin();  // Reset HP20x_dev
+    delay(100);
+  
+    /* Determine HP20x_dev is available or not */
+    ret = HP20x.isAvailable();
+    if(OK_HP20X_DEV == ret){
+      Serial.println("HP20x_dev is available.");    
+    } else {
+      Serial.println("HP20x_dev isn't available.");
+    }
+
     
     // LinkIt One WiFi
-    // check for the presence of the shield:
-    
     LWiFi.begin();
-    Serial.print("Connecting to WiFi AP: ");
-    Serial.println(WIFI_AP);
-    while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD))){
-      Serial.println("Fail and try it again...");
-      delay(500);
-    }
-    Serial.print("My IP is: ");
-    Serial.println(LWiFi.localIP());
-    
 }
 
 void loop()
@@ -109,6 +95,7 @@ int v_int;
 long v_long;
 long light;
 float ba, bp;
+
  
     lcd.setRGB(0,0,0);
 
@@ -116,11 +103,10 @@ float ba, bp;
     light = TSL2561.readVisibleLux();
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("light: ");
-    lcd.print(light);
+    lcd.print("light: " + String(light));
 
     if(light<750){
-      lcd.setRGB(0,0,255);
+      lcd.setRGB(random(128)+random(128), random(128)+random(128), random(128)+random(128));
     } else {
       lcd.setRGB(0,0,0);
     }
@@ -128,93 +114,175 @@ float ba, bp;
     delay(2000);
 
     // Grove - Temperature and Humidity Sensor Pro
-    // http://www.seeedstudio.com/wiki/Grove_-_Temperature_and_Humidity_Sensor_Pro
-//    float h = dht.readHumidity();
-//    float t = dht.readTemperature();
-    // for LinkIt One, use the library: https://github.com/Seeed-Studio/Grove_Starter_Kit_For_LinkIt/tree/master/libraries/Humidity_Temperature_Sensor
-    
+    // use the library: https://github.com/Seeed-Studio/Grove_Starter_Kit_For_LinkIt/tree/master/libraries/Humidity_Temperature_Sensor    
     float h,t;
     dht.readHT(&t, &h);
-    while (isnan(t) || isnan(h)){
+    while (isnan(t) || isnan(h) || t<0 || t>80 || h<0 || h > 100){
+      Serial.println("Something wrong with DHT => retry it!");
+      delay(100);
       dht.readHT(&t, &h);    
     }
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("temp: ");
-      lcd.print(t);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("temp: " + String(t));
     
-      lcd.setCursor(0,1);
-      lcd.print("humid: ");
-      lcd.print(h);
-      
-
-
-    // Wait one second between measurements.
+    lcd.setCursor(0,1);
+    lcd.print("humid: " + String(h));
+  
+    // Wait a bit between measurements.
     delay(2000);
 
     // Grove - Barometer (High-Accuracy)
     if(OK_HP20X_DEV == ret){ 
       //v_long = HP20x.ReadTemperature();
       //float bt = v_long/100.0;
-      v_long = HP20x.ReadPressure();
-      bp = v_long/100.0;
-      v_long = HP20x.ReadAltitude();
-      ba = v_long/100.0;
-
+      while(1){
+        v_long = HP20x.ReadPressure();
+        bp = v_long/100.0;
+        v_long = HP20x.ReadAltitude();
+        ba = v_long/100.0;
+        if (bp>10) break;
+        Serial.println("Something wrong with barometer => retry it!");
+      }
+      
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("pressure: ");
-      //lcd.print(bt);
-      //lcd.print(" ");
-      lcd.print(bp);
-      
+      lcd.print("pressure: " + String(bp));
       lcd.setCursor(0, 1);
-      lcd.print("altitude: ");
-      lcd.print(ba);
+      lcd.print("altitude: " + String(ba));
 
       delay(2000);
     }
 
-
-      String data = "field1="+String(t)+"&field2="+String(h)+"&field3="+String(bp)+"&field4="+String(light);
-      String data_len = String(data.length());
-      Serial.println("Data: "+data);
-      
-    Serial.print("Connecting to site...");
-    if(!c.connect("api.thingspeak.com", 80)){
-      Serial.println("FAIL!");
-      Serial.println(LWiFi.localIP());
+    // Show battery status
+    v_int = LBattery.level();
+    if (v_int < 50) { lcd.setRGB(255,0,0); }
+    lcd.clear();
+    if (LBattery.isCharging()){
+      Serial.println("Charging Mode-- " + String(v_int, DEC) + "%");
+      lcd.setCursor(0, 0);
+      lcd.print(".. Charging ..");
     } else {
-      c.println("POST /update HTTP/1.1");
-      c.println("Host: api.thingspeak.com");
-      c.println("Connection: close");
-      c.println("X-THINGSPEAKAPIKEY: B4MIQ67R4DJWIK7U");
-      c.println("Content-Type: application/x-www-form-urlencoded");
-      c.println("Content-Length: " + data_len);
-      c.println();
-      c.println(data);
-      
+      Serial.println("Battery Mode -- " + String(v_int, DEC) + "%");
+      lcd.setCursor(0, 0);
+      lcd.print("On Battery");
+    }
+    lcd.setCursor(0, 1);
+    lcd.print("Battery: " + String(v_int, DEC) + "%");
+    delay(2000);
 
-      // read server response
-      while (c){
-        Serial.print((char)c.read());
-      }
-      c.stop();
-      delay(5000);
+
+    // upload data to thingspeak
+    // reference: https://github.com/iobridge/ThingSpeak-Arduino-Examples/blob/master/Ethernet/Arduino_to_ThingSpeak.ino
+    while (client.available()){
+      char c = client.read();
+      Serial.print(c);
+    }
+
+    // Disconnect from ThingSpeak
+    if (!client.connected() && lastConnected){
+      Serial.println("...disconnected");
+      Serial.println();
+      client.stop();
+    }
+  
+    // Update ThingSpeak
+    if((millis() - lastConnectionTime > updateThingSpeakInterval)){
+      Serial.println("about to update ThingSpeak...");
+      updateThingSpeak("field1="+String(t)+"&field2="+String(h)+"&field3="+String(bp)+"&field4="+String(light)+"&field5="+String(v_int));
+    }
+  
+    // Check if LWiFi needs to be restarted
+    if (failedCounter > 3 ) { 
+      start_WiFi(); 
+    }
+  
+    lastConnected = client.connected();
+    
+}
+
+//=======================================
+// reset WiFi connection
+//=======================================
+void start_WiFi(){
+    if (client.connected()) { 
+      client.stop(); 
+      client.flush();
+    }
+    Serial.println("Connecting to WiFi AP: " + String(WIFI_AP));
+    Serial.println(String(hour(),DEC) + ":" + String(minute(),DEC) + ":" + String(second(),DEC) + "  start start_WiFi()");
+
+    if (0 <= LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD))){
+      Serial.println("WiFi connection failed.....");
     }
     
+    wifistatus = LWiFi.status();
+    if (wifistatus == LWIFI_STATUS_DISABLED){
+      Serial.println("WiFi status: LWIFI_STATUS_DISABLED");
+    } else if (wifistatus == LWIFI_STATUS_DISCONNECTED) {
+      Serial.println("WiFi status: LWIFI_STATUS_DISCONNECTED");
+    } else {
+      Serial.println("WiFi status: LWIFI_STATUS_CONNECTED");
+      Serial.print("My IP is: ");
+      Serial.println(LWiFi.localIP());
+      Serial.print("RSSI is: ");
+      Serial.println(LWiFi.RSSI());
+      failedCounter = 0;
+    }
+}
 
+//=======================================
+//  update ThingSpeak
+//  Reference: https://github.com/iobridge/ThingSpeak-Arduino-Examples/blob/master/Ethernet/Arduino_to_ThingSpeak.ino
+//=======================================
+void updateThingSpeak(String tsData){
+
+  wifistatus = LWiFi.status();
+  if (wifistatus == LWIFI_STATUS_DISABLED){
+    Serial.println("=> WiFi status: LWIFI_STATUS_DISABLED");
+    LWiFi.begin();
+    start_WiFi();
+  } else if (wifistatus == LWIFI_STATUS_DISCONNECTED) {
+    Serial.println("=> WiFi status: LWIFI_STATUS_DISCONNECTED");
+    start_WiFi();
+  } else {
+    Serial.println("=> WiFi status: LWIFI_STATUS_CONNECTED");
+    Serial.print("=> My IP is: ");
+    Serial.println(LWiFi.localIP());
+    Serial.print("RSSI is: ");
+    Serial.println(LWiFi.RSSI());
+  }
     
+  if (wifistatus != LWIFI_STATUS_CONNECTED){
+    failedCounter++;
+    Serial.println("Did not try updateThingSpeak because it's not connected....");
+    Serial.println();
+    return;
+  }
+  
+  if (client.connect("api.thingspeak.com", 80)){         
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: "+THINGSPEAK_API_KEY+"\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+    client.print(tsData);
     
-    // Grove Kit Sensor: temperature 
-    //v_int = analogRead(pinTemp);
-    //float resistance = (float)(1023-v_int)*10000/v_int;
-    //float temperature = 1/(log(resistance/10000)/B+1/298.15)-273.15;
-    // Grove Kit Sensor: light 
-    //v_int = analogRead(pinLight);
-    //lcd.clear();
-    //lcd.setCursor(0, 0);
-    //lcd.print("light: ");
-    //lcd.print(v_int);
+    lastConnectionTime = millis();
     
+    if (client.connected()){
+      Serial.println("Connecting to ThingSpeak...");      
+      failedCounter = 0;
+    } else {
+      failedCounter++;
+      Serial.println("Connection to ThingSpeak failed ("+String(failedCounter, DEC)+")");   
+    }
+  } else {
+    failedCounter++;  
+    Serial.println("Connection to ThingSpeak Failed (("+String(failedCounter, DEC)+"))");   
+    lastConnectionTime = millis(); 
+  }
 }
